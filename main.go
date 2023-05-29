e9d06c78fddefa5a30db794e4128ea1bfb6f0efe
Abdullah GÜNGÖR <abdullahgungor@hotmail.com.tr>
2023-05-29 14:02:05 +0300

    publishing code added

--- main.go
+++ main.go
@@ -10,10 +10,45 @@ func main() {
 	fmt.Println("Go RabbitMQ Tutorial")
 	conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
 	if err != nil {
-		fmt.Println(err)
-		panic(1)
+		fmt.Println("Failed Initializing Broker Connection")
+		panic(err)
 	}
-	defer conn.Close()
 
-	fmt.Println("Successfully Connected to our RabbitMQ Instance")
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer ch.Close()
+
+	q, err := ch.QueueDeclare(
+		"TestQueue",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	)
+
+	fmt.Println(q)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	err = ch.Publish(
+		"",
+		"TestQueue",
+		false,
+		false,
+		amqp.Publishing{
+			ContentType: "text/plain",
+			Body:        []byte("Hello World"),
+		},
+	)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println("Successfully Published Message to Queue")
+
 }
